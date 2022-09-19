4dbaa0a267b7d7c7ea6447b1ac434b256e3206c5
roy-pmgrow <yi.kim@pmgrow.co.kr>
2022-09-19 14:43:03 +0900

    ✨ Feat: 웹뷰 당겨서 새로고침 기능 추가

--- app/src/main/java/com/pmgrow/baas/baasmobile/MainActivity.kt
+++ app/src/main/java/com/pmgrow/baas/baasmobile/MainActivity.kt
@@ -2,8 +2,11 @@ package com.pmgrow.baas.baasmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
+import android.webkit.ConsoleMessage
+import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import com.pmgrow.baas.baasmobile.databinding.ActivityMainBinding
@@ -19,6 +22,20 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.webview.webViewClient = WebViewClient()
         binding.webview.settings.javaScriptEnabled = true
+        binding.webview.settings.domStorageEnabled = true
         binding.webview.loadUrl("http://192.168.0.143:3000/")
+        binding.webview.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+                consoleMessage?.apply {
+                    Log.d("MyApplication", "${message()} -- From line ${lineNumber()} of ${sourceId()}")
+                }
+                return true
+            }
+        }
+
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            binding.webview.reload();
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
     }
 }
